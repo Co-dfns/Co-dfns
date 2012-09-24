7fada42996a316ad933a3fa7da60738179e41420
Aaron W. Hsu <arcfide@sacrideo.us>
2012-09-23 22:20:51 -0400

    Finish up some of the main things about catenation, though its missing indexing

--- isabelle/HPAPL/HPAPL.thy
+++ isabelle/HPAPL/HPAPL.thy
@@ -490,190 +490,89 @@ lemma reshape_index_vec [simp]:
   "array_get i (reshape s (Vector v)) = v ! (gamma i s) mod (total (Vector v))"
 *)
 
-subsubsection {* Catenating arrays *}
+subsubsection {* Catenating Vectors *}
 
+text {*
+Catenating two vectors is the simpler operation than catenating full 
+blown arrays. Thus, catenation of vectors will be defined here first, and 
+later section will deal with the issues of extending this definition to 
+handle higher dimension arrays.
 
+Catenation of vectors definition looks like this:
+*}
+
+definition catvec :: "'a::scalar array \<Rightarrow> 'a array \<Rightarrow> 'a array" where
+  "catvec a b \<equiv> 
+    (Array [length (valuelst a) + (length (valuelst b))]
+           (valuelst a @ valuelst b))"
+
+text {*
+The following lemma describes how indexing behaves on catenated vectors.
+(Ed: These indexing ones are a little tricky, as above, so I am not going 
+to prove this one right now.)
+*}
 
 (*
-
-lemma valuelst_length [simp]: "listprod (shapelst a) = (length (valuelst a))"
-by (simp add: valuelst_def shapelst_def) (cases a, auto)
-
-lemma natrank_Empty [simp]: "natrank Empty = 1"
-by (simp add: Empty_def natrank_def shapelst_def)
-
-definition vec2lst :: "'a::scalar array \<Rightarrow> 'a::scalar list"
-where "vec2lst vec \<equiv> case vec of Array s v \<Rightarrow> v"
-
-theorem vector_shape [simp]: "shapelst (Vector s) = [length s]"
-by (simp add: Vector_def shapelst_def)
-
-theorem vector_inverse [simp]: "vec2lst (Vector lst) = lst"
-by (simp add: Vector_def vec2lst_def)
-
-theorem vector_empty: "Vector [] = Empty"
-by (simp add: Vector_def Empty_def)
-
-theorem vec2lst_empty [simp]: "vec2lst Empty = []"
-by (simp add: Empty_def vec2lst_def)
-
-theorem rank_shape_one [simp]: "natrank (shape a) = 1" 
-by (auto simp: shape_def)
-
-theorem shape2_rank [simp]: "shape (shape a) = Vector [natrank a]"
-by (auto simp: shape_def natrank_def)
-
-theorem shape3_is_one [simp]: "shape (shape (shape a)) = Vector [1]"
-by (auto simp: shape_def)
-
-theorem shape_scalar [simp]: "shape (Scalar x) = Empty"
-by (simp add: shape_def Empty_def Vector_def Scalar_def shapelst_def)
-
-fun comp_idx :: "nat \<Rightarrow> nat list \<Rightarrow> nat list \<Rightarrow> nat" where
-    "comp_idx n [] i = n"
-  | "comp_idx n s [] = n"
-  | "comp_idx n (se # sr) (ie # ir) = comp_idx (ie + se * n) sr ir"
-
-definition index_unraveled :: "nat list \<Rightarrow> nat list \<Rightarrow> nat" 
-where "index_unraveled s i \<equiv> comp_idx 0 s i"
-
-definition index :: "nat array \<Rightarrow> 'a::scalar array \<Rightarrow> 'a" where
-"index i a \<equiv> (valuelst a) ! index_unraveled (shapelst a) (valuelst i)"
-
-theorem index_scalar [simp]: "index Empty (Scalar x) = x"
-by (simp add: Empty_def Scalar_def index_def valuelst_def shapelst_def
-              listprod_def index_unraveled_def)
-
-theorem index_vector [simp]: 
-  "i < length l \<Longrightarrow> index (Vector [i]) (Vector l) = l ! i"
-by (simp add: index_def Vector_def valuelst_def shapelst_def listprod_def
-                 index_unraveled_def)
-
-definition arrequal :: "'a::scalar array \<Rightarrow> 'a array \<Rightarrow> bool" where 
-  "arrequal a b \<equiv> (shapelst a) = (shapelst b) \<and> (valuelst a) = (valuelst b)"
-
-definition reshape :: "nat array \<Rightarrow> 'a::scalar array \<Rightarrow> 'a array"
-where "reshape ns a \<equiv> Array (valuelst ns) (valuelst a)"
-
-theorem shape_preserved [simp]: "arrequal a (reshape (shape a) a)"
-by (simp add: arrequal_def reshape_def shape_def) (simp add: valuelst_def)
-
-definition index_gen :: "nat array \<Rightarrow> nat array" where 
-  "index_gen a \<equiv>
-     case a of
-         Array [] x \<Rightarrow> Array [hd (valuelst a)] (upt 0 (hd (valuelst a)))
-       | Array [n] x \<Rightarrow> Empty"
-(*
-       | Array [n] x \<Rightarrow> Array [(listprod (valuelst a)), n] 
-                         (flatten (map (idx_lst (valuelst a)) 
-                                       [0..<listprod (valuelst a)]))"
+lemma catvec_index:
+  "array_get (Vector [i]) (catvec (Vector x) (Vector y))
+     = (if (0 \<le> i \<and> i < (total (Vector x)))
+       then array_get (Vector [i]) (Vector x)
+       else array_get (Vector [i - (total (Vector x))]) (Vector y))"
 *)
 
-theorem index_gen_scl_len [simp]: 
-  "length (valuelst (index_gen (Scalar n))) = n"
-by (simp add: index_gen_def Scalar_def valuelst_def listprod_def)
+text {*
+The number of elements in a catenated array is a function of the 
+elements in the arguments, stated as the following lemma.
+*}
 
-lemma sv2vl_lst [simp]: "sv2vl n x [0..<n] = [0..<n]"
-proof -
-  have "sv2vl n x [0..<n] = (sv2vl (length [0..<n]) x [0..<n])" by simp
-  also have "... = [0..<n]" unfolding sv2vl_id ..
-  finally show "sv2vl n x [0..<n] = ([0..<n])" .
-qed
+lemma catvec_total [simp]: 
+  "total (catvec (Vector x) (Vector y)) = (total (Vector x)) + (total (Vector y))"
+by (simp add: catvec_def total_def shape_def Vector_def prod_def valuelst_def)
 
-theorem index_gen_scl_vl [simp]:
-  "(valuelst (index_gen (Scalar n))) = (upt 0 n)"
-by (simp add: index_gen_def Scalar_def valuelst_def listprod_def)
+text {*
+Thus the shape of a catvec can be described using the following lemma.
+*}
 
-theorem index_gen_scl_shp [simp]:
-  "(shapelst (index_gen (Scalar n))) = [n]"
-by (simp add: index_gen_def Scalar_def valuelst_def listprod_def)
+lemma catvec_shape [simp]:
+  "shape (catvec (Vector x) (Vector y)) 
+     = (Vector [((total (Vector x)) + (total (Vector y)))])"
+by (simp add: catvec_def Vector_def shape_def total_def prod_def valuelst_def)
 
-definition msop :: "('a::scalar \<Rightarrow> 'b::scalar) \<Rightarrow> 'a array \<Rightarrow> 'b array" 
-where "msop f a \<equiv> Array (shapelst a) (map f (valuelst a))"
+text {*
+Vector catenation obeys the following common properties.
+*}
 
-fun dsop :: "('a::scalar \<Rightarrow> 'b::scalar \<Rightarrow> 'c::scalar)
-                         \<Rightarrow> 'a array \<Rightarrow> 'b array \<Rightarrow> 'c array"
-where 
-    "dsop f (Array [] a) (Array [] b) =
-      (Array [] 
-        [f av bv. 
-         av \<leftarrow> (valuelst (Array [] a)), bv \<leftarrow> (valuelst (Array [] b))])"
-  | "dsop f (Array (x # xs) a) (Array [] b) =
-      (Array (x # xs) 
-        [f av bv. 
-         av \<leftarrow> (valuelst (Array (x # xs) a)),
-         bv \<leftarrow> (valuelst (Array (x # xs) (valuelst (Array [] b))))])"
-  | "dsop f (Array [] a) (Array (x # xs) b) =
-      (Array (x # xs)
-        [f av bv.
-         av \<leftarrow> (valuelst (Array (x # xs) (valuelst (Array [] a)))),
-         bv \<leftarrow> (valuelst (Array (x # xs) b))])"
-  | "dsop f a b = 
-      (Array (shapelst a) [f av bv. av \<leftarrow> (valuelst a), bv \<leftarrow> (valuelst b)])"
+lemma catvec_zero [simp]: "catvec Empty (Vector x) = (Vector x)"
+by (simp add: Empty_def catvec_def Vector_def valuelst_def)
 
-definition firstval :: "'a::scalar array \<Rightarrow> 'a"
-where "firstval a \<equiv> hd (valuelst a)"
+lemma catvec_zero2 [simp]: "catvec (Vector x) Empty = (Vector x)"
+by (simp add: Empty_def catvec_def Vector_def valuelst_def)
 
-definition first :: "'a::scalar array \<Rightarrow> 'a array" 
-where "first a \<equiv> Array [] [firstval a]"
+lemma catvec_commute [simp]:
+  "catvec (Vector x) (catvec (Vector y) (Vector z))
+     = catvec (catvec (Vector x) (Vector y)) (Vector z)"
+by (auto simp: catvec_def valuelst_def Vector_def)
+   (metis append_assoc length_append sv2vl_id)
 
-definition msafun :: "('a::scalar array \<Rightarrow> 'b::scalar array) 
-                      \<Rightarrow> 'a \<Rightarrow> 'b"
-where "msafun f x \<equiv> firstval (f (Array [] [x]))"
+text {*
+The above definition of vector catenation is nice because it also allows 
+easy extension to handle scalars. The following lemmas follow from the 
+above definition, and specify the behavior of scalar catenation with 
+vectors as vector operations alone.
+*}
 
-definition dsafun :: "('a::scalar array \<Rightarrow> 'b::scalar array \<Rightarrow> 'c::scalar array)
-                      \<Rightarrow> 'a \<Rightarrow> 'b \<Rightarrow> 'c"
-where "dsafun f x y \<equiv> firstval (f (Array [] [x]) (Array [] [y]))"
+lemma catvec_scalar [simp]: 
+  "catvec (Scalar s) (Vector x) = catvec (Vector [s]) (Vector x)"
+by (simp add: catvec_def)
 
-definition eachm :: "('a::scalar array \<Rightarrow> 'b::scalar array)
-                     \<Rightarrow> 'a array \<Rightarrow> 'b array"
-where "eachm f a \<equiv> msop (msafun f) a"
+lemma catvec_scalar2 [simp]:
+  "catvec (Vector x) (Scalar s) = catvec (Vector x) (Vector [s])"
+by (simp add: catvec_def)
 
-definition eachd :: "('a::scalar array \<Rightarrow> 'b::scalar array \<Rightarrow> 'c::scalar array)
-                     \<Rightarrow> 'a array \<Rightarrow> 'b array \<Rightarrow> 'c array"
-where
-  "eachd f a b \<equiv> dsop (dsafun f) a b"
+lemma catvec_scalar3 [simp]:
+  "catvec (Scalar s) (Scalar t) = catvec (Vector [s]) (Vector [t])"
+by (simp add: catvec_def)
 
-primrec listscan :: "('a \<Rightarrow> 'a \<Rightarrow> 'a) \<Rightarrow> 'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where 
-    "listscan f s [] = []"
-  | "listscan f s (x # xs) = [foldr f s x] @ listscan f (s @ [x]) xs"
-
-definition scan :: "('a::scalar array \<Rightarrow> 'a::scalar array \<Rightarrow> 'a::scalar array) 
-                    \<Rightarrow> 'a array \<Rightarrow> 'a array"
-where "scan f a \<equiv> Array (shapelst a) (listscan (dsafun f) [] (valuelst a))"
-
-definition reduce :: "('a::scalar array \<Rightarrow> 'a::scalar array \<Rightarrow> 'a::scalar array)
-                      \<Rightarrow> 'a array \<Rightarrow> 'a array"
-where "reduce f a \<equiv> Array [] [last (valuelst (scan f a))]"
-*)
-(* datatype prog = Prog sig "stmt list"
-      and sig = Mon string string "string list"
-              | Dya string string string "string list"
-              | MonMonOp string string string "string list"
-              | MonDyaOp string string string string "string list"
-              | DyaMonOp string string string string "string list"
-              | DyaDyaOp string string string string string "string list"
-     and stmt = EStmt exp | AStmt string exp | FStmt string func
-              | OStmt string oper | SelStmt exp exp
-              | IfEStmt exp exp | IfAStmt exp string exp | IfFStmt string func
-              | IfOStmt string oper | IfSelStmt exp exp
-      and exp = ValNat "nat list" | ValInt "int list" | Var string 
-              | Zilde | Donut | Alpha | Omega | AlphaAlpha | OmegaOmega
-              | MApp func exp | DApp func exp exp 
-     and func = AnonF "stmt list" | FNam string | PrimF primf
-              | DFFOp oper func func | DFEOp oper func exp 
-              | DEFOp oper exp func | DEEOp oper exp exp
-              | MFOp oper func | MEOp oper exp
-     and oper = AnonO "stmt list" | ONam string | PrimO primo
-    and primf = Rho | Iota | Drop | Take | Index | First | Plus | And 
-              | Bang | Comma | Ceil | Floor | Circ | Hook | Decode | Encode 
-              | Equiv | Times | Div | Enclose | Memb | Eq | Tilde
-              | Expand | Exp | Find | GradeD | GradeU | GrThn | LsThn 
-              | GrThnEq | LsThnEq | LTack | RTack | Intersect | Union
-              | Log | Bar | Domino | Minus | Nand | Nor | NotEq 
-              | Or | Power | Replicate | Rot | Transp
-    and primo = Each | Reduce | Scan | Axis | Commute | Compose | InProd
-              | OutProd | PowerOp | Spawn | Parallel
-*)
+subsection {* Scalar functions over arrays *}
 
 end
