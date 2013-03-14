888c46844b2850cecff9ef48fb1a6dd2df819850
Aaron W. Hsu <arcfide@sacrideo.us>
2013-03-13 22:38:45 -0400

    Add support for function calls The compiler now handles a single function call inside of the expression body of a function. It still needs to be a function with constant inputs, but the work is in place for a more generalized expression handler. The main limitation now is that the expression body still assumes a single expression in the function. This will require a new pass to unnest the expressions, and this will further require better handling of basic blocks in the generate_llvm() pass. Moreover, we are now at the point where we will want to have some sort of harness for testing the results actually run correctly, and this will require some more hacking and a runtime.

--- src/generate_llvm.c
+++ src/generate_llvm.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <llvm-c/Core.h>
 
 #include "pool.h"
@@ -33,11 +36,78 @@ constant_type(void)
 LLVMTypeRef
 function_type(void)
 {
-	LLVMTypeRef p;
+	LLVMTypeRef p, pts[2];
 
 	p = LLVMPointerType(constant_type(), 0);
+	pts[0] = p;
+	pts[1] = p;
 
-	return LLVMFunctionType(p, NULL, 0, 0);
+	return LLVMFunctionType(p, pts, 2, 0);
+}
+
+LLVMValueRef
+primitive_function(LLVMModuleRef m, enum primitive fn)
+{
+	LLVMTypeRef t;
+
+	t = function_type();
+
+	switch (fn) {
+	case PRM_MINUS: return LLVMAddFunction(m, "codfns_minus", t);
+	case PRM_PLUS: return LLVMAddFunction(m, "codfns_plus", t);
+	case PRM_LT: return LLVMAddFunction(m, "codfns_lt", t);
+	case PRM_LTE: return LLVMAddFunction(m, "codfns_lte", t);
+	case PRM_EQ: return LLVMAddFunction(m, "codfns_eq", t);
+	case PRM_GTE: return LLVMAddFunction(m, "codfns_gte", t);
+	case PRM_GT: return LLVMAddFunction(m, "codfns_gt", t);
+	case PRM_NEQ: return LLVMAddFunction(m, "codfns_neq", t);
+	case PRM_AND: return LLVMAddFunction(m, "codfns_and", t);
+	case PRM_OR: return LLVMAddFunction(m, "codfns_or", t);
+	case PRM_TIMES: return LLVMAddFunction(m, "codfns_times", t);
+	case PRM_DIV: return LLVMAddFunction(m, "codfns_div", t);
+	case PRM_HOOK: return LLVMAddFunction(m, "codfns_hook", t);
+	case PRM_MEM: return LLVMAddFunction(m, "codfns_mem", t);
+	case PRM_RHO: return LLVMAddFunction(m, "codfns_rho", t);
+	case PRM_NOT: return LLVMAddFunction(m, "codfns_not", t);
+	case PRM_TAKE: return LLVMAddFunction(m, "codfns_take", t);
+	case PRM_DROP: return LLVMAddFunction(m, "codfns_drop", t);
+	case PRM_IOTA: return LLVMAddFunction(m, "codfns_iota", t);
+	case PRM_CIRC: return LLVMAddFunction(m, "codfns_circ", t);
+	case PRM_POW: return LLVMAddFunction(m, "codfns_pow", t);
+	case PRM_CEIL: return LLVMAddFunction(m, "codfns_ceil", t);
+	case PRM_FLOOR: return LLVMAddFunction(m, "codfns_floor", t);
+	case PRM_DEL: return LLVMAddFunction(m, "codfns_del", t);
+	case PRM_RGT: return LLVMAddFunction(m, "codfns_rgt", t);
+	case PRM_LFT: return LLVMAddFunction(m, "codfns_lft", t);
+	case PRM_ENCL: return LLVMAddFunction(m, "codfns_encl", t);
+	case PRM_DIS: return LLVMAddFunction(m, "codfns_dis", t);
+	case PRM_INTER: return LLVMAddFunction(m, "codfns_inter", t);
+	case PRM_UNION: return LLVMAddFunction(m, "codfns_union", t);
+	case PRM_ENC: return LLVMAddFunction(m, "codfns_enc", t);
+	case PRM_DEC: return LLVMAddFunction(m, "codfns_dec", t);
+	case PRM_ABS: return LLVMAddFunction(m, "codfns_abs", t);
+	case PRM_EXPNF: return LLVMAddFunction(m, "codfns_expnf", t);
+	case PRM_FILF: return LLVMAddFunction(m, "codfns_filf", t);
+	case PRM_GRDD: return LLVMAddFunction(m, "codfns_grdd", t);
+	case PRM_GRDU: return LLVMAddFunction(m, "codfns_grdu", t);
+	case PRM_ROT: return LLVMAddFunction(m, "codfns_rot", t);
+	case PRM_TRANS: return LLVMAddFunction(m, "codfns_trans", t);
+	case PRM_ROTF: return LLVMAddFunction(m, "codfns_rotf", t);
+	case PRM_LOG: return LLVMAddFunction(m, "codfns_log", t);
+	case PRM_NAND: return LLVMAddFunction(m, "codfns_nand", t);
+	case PRM_NOR: return LLVMAddFunction(m, "codfns_nor", t);
+	case PRM_BANG: return LLVMAddFunction(m, "codfns_bang", t);
+	case PRM_MDIV: return LLVMAddFunction(m, "codfns_mdiv", t);
+	case PRM_FIND: return LLVMAddFunction(m, "codfns_find", t);
+	case PRM_SQUAD: return LLVMAddFunction(m, "codfns_squad", t);
+	case PRM_EQV: return LLVMAddFunction(m, "codfns_eqv", t);
+	case PRM_NEQV: return LLVMAddFunction(m, "codfns_neqv", t);
+	case PRM_CATF: return LLVMAddFunction(m, "codfns_catf", t);
+	case PRM_FIL: return LLVMAddFunction(m, "codfns_fil", t);
+	case PRM_EXPND: return LLVMAddFunction(m, "codfns_expnd", t);
+	case PRM_CAT: return LLVMAddFunction(m, "codfns_cat", t);
+	case PRM_HAT: return LLVMAddFunction(m, "codfns_hat", t);
+	}
 }
 
 LLVMValueRef
@@ -77,23 +147,71 @@ gl_constant(Pool *p, LLVMModuleRef m, Constant *v)
 	return LLVMConstStruct(r, 4, 0);
 }
 
+LLVMValueRef
+gl_application(LLVMModuleRef m, LLVMBuilderRef bldr, Application *a)
+{
+	Expression *el, *er;
+	char *lft, *rgt;
+	enum primitive prm;
+	LLVMValueRef args[2], fn, call;
+
+	prm = a->fn;
+	el = a->lft;
+	er = a->rgt;
+	
+	lft = el == NULL ? NULL : ((Variable *) el->value)->name;
+	rgt = ((Variable *) el->value)->name;
+
+	if (lft == NULL) {
+		args[0] = LLVMConstPointerNull(constant_type());
+	} else {
+		args[0] = LLVMGetNamedGlobal(m, lft);
+	}
+
+	args[1] = LLVMGetNamedGlobal(m, rgt);
+	fn = primitive_function(m, prm);
+	call = LLVMBuildCall(bldr, fn, args, 2, "");
+	
+	return call;
+}
+
+void
+gl_expression(LLVMModuleRef m, LLVMBuilderRef bldr, Expression *e)
+{
+	char *vn;
+	LLVMValueRef res;
+	
+	switch (e->type) {
+	case EXPR_VAR:
+		vn = ((Variable *) e->value)->name;
+		res = LLVMGetNamedGlobal(m, vn);
+		break;
+	case EXPR_APP:
+		res = gl_application(m, bldr, e->value);
+		break;
+	case EXPR_LIT:	
+		fprintf(stderr, "EXPR_LIT in gl_function; this should never happen\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	LLVMBuildRet(bldr, res);
+}
+
 void
 gl_function(LLVMModuleRef m, LLVMValueRef lf, Function *fn)
 {
-	char *vn;
+	Expression *e;
 	LLVMBasicBlockRef bb;
 	LLVMBuilderRef bldr;
-	LLVMValueRef g;
 	
 	bldr = LLVMCreateBuilder();
 	bb = LLVMAppendBasicBlock(lf, "_");
+	LLVMPositionBuilderAtEnd(bldr, bb);
 
 	/* For now we assume that we have one variable expression */
-	vn = ((Variable *) fn->stmts[0]->value)->name;
-	g = LLVMGetNamedGlobal(m, vn);
-	
-	LLVMPositionBuilderAtEnd(bldr, bb);
-	LLVMBuildRet(bldr, g);
+	e = fn->stmts[0];
+	gl_expression(m, bldr, e);
+
 	LLVMDisposeBuilder(bldr);
 }
 
