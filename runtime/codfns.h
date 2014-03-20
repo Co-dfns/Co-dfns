39761a77c57c122e8d5bd9f56984988234cb9c16
Tim Zakian <tzakian@indiana.edu>
2014-03-19 20:29:37 -0400

    Adding runtime. Couldn't figure out what the weird dot/lower-perp/upper-perp do (according to the dyalog docs) so those aren't implemented. Runtime now has a dependency on math.h (due to exp, pow and abs)

--- runtime/codfns.h
+++ runtime/codfns.h
@@ -18,3 +18,23 @@ int FFIMakeArray(struct codfns_array **res,
     uint16_t rnk, uint64_t sz, uint32_t *shp, int64_t *dat);
 void array_free(struct codfns_array *arr);
 int array_cp(struct codfns_array *tgt, struct codfns_array *src);
+
+int codfns_same_shape(struct codfns_array *, struct codfns_array *); 
+int codfns_add(struct codfns_array *, struct codfns_array *, 
+    struct codfns_array *);
+int codfns_subtract(struct codfns_array *, struct codfns_array *, 
+    struct codfns_array *);
+int codfns_multiply(struct codfns_array *, 
+    struct codfns_array *, struct codfns_array *);
+int codfns_divide(struct codfns_array *, 
+    struct codfns_array *, struct codfns_array *, int );
+int codfns_not(struct codfns_array *, 
+    struct codfns_array *, struct codfns_array *); 
+int codfns_less(struct codfns_array *, struct codfns_array *, struct codfns_array *);
+int codfns_less_or_equal(struct codfns_array *, struct codfns_array *, struct codfns_array *);
+int codfns_equal(struct codfns_array *, struct codfns_array *, struct codfns_array *);
+int codfns_greater_or_equal(struct codfns_array *, struct codfns_array *, struct codfns_array *);
+int codfns_not_equal(struct codfns_array *, struct codfns_array *, struct codfns_array *);
+int codfns_power(struct codfns_array *ret, struct codfns_array *lft, struct codfns_array *rgt);
+int codfns_magnitude(struct codfns_array *ret, struct codfns_array *lft, struct codfns_array *rgt);
+
