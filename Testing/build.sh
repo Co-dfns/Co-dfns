ce56704ffd73038296879657f89b7bed7fb70a8e
Aaron W. Hsu <arcfide@sacrideo.us>
2015-06-05 18:41:27 -0400

    All the best stuff working now.

--- Testing/build.sh
+++ Testing/build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-#FEATURES="-funsigned-bitfields -funsigned-char -fvisibility=hidden -fPIC"
-FEATURES=""
+FEATURES="-funsigned-bitfields -funsigned-char -fvisibility=hidden -fPIC"
+#FEATURES=""
 DEFINES="-DxxBIT=64 -DHAS_UNICODE=1 -DUNIX=1 -DWANT_REFCOUNTS=1 -D_DEBUG=1"
 INCLUDES="-I./dwa"
-#FLAGS="-fast -fno-alias -prof-use -shared -static-intel -Wall -Wno-unused-function"
-FLAGS="-fastsse -shared -fPIC -acc"
+FLAGS="-fast -fno-alias -shared -static-intel -Wall -Wno-unused-function"
+#FLAGS="-fastsse -shared -fPIC -acc"
 FILES="dwa/dwa_fns.c Scratch/blackscholes_tweak.c Scratch/bs_c.c"
-OUTPUT="Scratch/blackscholes.so"
-#CC=icc
-CC=pgcc
+OUTPUT="Scratch/blackscholes2.so"
+CC=icc
+#CC=pgcc
+LD_LIBRARY_PATH=/opt/intel/lib/intel64
 
 echo "$CC $FEATURES $DEFINES $INCLUDES $FLAGS -o $OUTPUT $FILES"
 $CC $FEATURES $DEFINES $INCLUDES $FLAGS -o $OUTPUT $FILES
