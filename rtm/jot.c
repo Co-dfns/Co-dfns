ec9b5c7926b75147c9d20148c45ddd76df640bbc
Aaron W. Hsu <arcfide@sacrideo.us>
2018-10-20 00:18:06 -0400

    Work by reference with free variables.

--- rtm/jot.c
+++ rtm/jot.c
@@ -1,6 +1,4 @@
 ﻿OD(jot,"jot",(scm(l)&&scm(r)),(scd(l)&&scd(r)),MFD,DFD)
-#define jotop(zz,ll,rr) jot_o zz(ll,rr)
-
 MF(jot_o){if(!fl){rr(z,aa,r);R;}if(!fr){ll(z,r,ww);R;}
  rr(z,r);ll(z,z);}
 DF(jot_o){if(!fl||!fr){err(2);}rr(z,r);ll(z,l,z);}
