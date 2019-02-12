2384a5f04ca8153e133dff30b5f9b956d95e3ed4
Aaron W. Hsu <arcfide@sacrideo.us>
2019-02-11 20:22:49 -0500

    Define stack and environment types

--- rtm/1obj.c
+++ rtm/1obj.c
@@ -3,7 +3,6 @@
 typedef long long L;typedef int I;typedef int16_t S16;typedef int8_t S8;
 typedef double D;typedef unsigned char U8;typedef unsigned U;
 typedef dim_t B;typedef cdouble DZ;typedef void V;typedef std::string STR;
-
 S{U f=3;U n;U x=0;wchar_t*v=L"Co-dfns";const wchar_t*e;V*c;}dmx;
 S lp{S{L l;B c;U t:4;U r:4;U e:4;U _:13;U _1:16;U _2:16;B s[1];}*p;};
 S dwa{B z;S{B z;V*(*ga)(U,U,B*,S lp*);V(*p[16])();V(*er)(V*);}*ws;V*p[4];};
@@ -11,19 +10,20 @@ S dwa*dwafns;Z V derr(U n){dmx.n=n;dwafns->ws->er(&dmx);}
 EXPORT I DyalogGetInterpreterFunctions(dwa*p){
  if(p)dwafns=p;else R 0;if(dwafns->z<sizeof(S dwa))R 16;R 0;}
 Z V err(U n,wchar_t*e){dmx.e=e;throw n;}Z V err(U n){dmx.e=L"";throw n;}
-S A{I r,f;dim4 s;array v;A(I r,dim4 s,array v):r(r),f(1),s(s),v(v){}
- A():r(0),f(0),s(dim4()),v(array()){}};
 dim4 eshp=dim4(0,(B*)NULL);
 std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> strconv;
-std::wstring msg;
-
-S FN{STR nm;I sm;I sd;FN&init;FN(STR nm,I sm,I sd,FN&init):nm(nm),sm(sm),sd(sd){}
+std::wstring msg;union BX;
+typedef std::vector<BX> FRM;typedef std::vector<FRM> ENV;
+typedef std::stack<BX> STK;
+S A{I r,f;dim4 s;array v;A(I r,dim4 s,array v):r(r),f(1),s(s),v(v){}
+ A():r(0),f(0),s(dim4()),v(array()){}};
+S FN{STR nm;I sm;I sd;FN(STR nm,I sm,I sd):nm(nm),sm(sm),sd(sd){}
  FN():nm(""),sm(0),sd(0){}
  virtual array id(dim4 s){err(16);R array();}
- virtual V operator()(A&z,const A&r){err(99);}
- virtual V operator()(A&z,const A&r,D ax){err(99);}
- virtual V operator()(A&z,const A&l,const A&r){err(99);}
- virtual V operator()(A&z,const A&l,const A&r,D ax){err(99);}};
+ virtual V operator()(A&z,const A&r,ENV&e){err(99);}
+ virtual V operator()(A&z,const A&r,D ax,ENV&e){err(99);}
+ virtual V operator()(A&z,const A&l,const A&r,ENV&e){err(99);}
+ virtual V operator()(A&z,const A&l,const A&r,D ax,ENV&e){err(99);}};
 FN MTFN;
 S MOP:FN{FN&ll;
  MOP(STR nm,I sm,I sd,FN&ll):FN(nm,sm,sd),ll(ll){}};
@@ -34,3 +34,4 @@ S DOP:FN{I fl;I fr;FN&ll;A aa;FN&rr;A ww;
   :FN(nm,sm,sd),fl(0),fr(1),ll(MTFN),aa(l),rr(r),ww(A()){}
  DOP(STR nm,I sm,I sd,FN&l,A r)
   :FN(nm,sm,sd),fl(1),fr(0),ll(l),aa(A()),rr(MTFN),ww(r){}};
+union BX{A v;FN f;BX(const FN&f):f(f){}BX(const A&v):v(v){}public:~BX();};
