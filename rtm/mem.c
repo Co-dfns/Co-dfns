c610c770436b78681fd370493a57cafabdceb4fc
Aaron W. Hsu <arcfide@sacrideo.us>
2021-02-09 02:54:41 -0500

    Update mem.c

--- rtm/mem.c
+++ rtm/mem.c
@@ -2,8 +2,10 @@
 DEFN(mem)
 MF(mem_f){z.s=SHP(1,cnt(r));z.v=r.v;}
 DF(mem_f){z.s=l.s;B lc=cnt(z);if(!lc){z.v=scl(0);R;}
- if(!cnt(r)){z.v=arr(lc,b8);z.v=0;R;}
- arr y=setUnique(r.v);B rc=y.elements();
- arr x=arr(l.v,lc,1);y=arr(y,1,rc);
+ if(!cnt(r)){arr zv(lc,b8);zv=0;z.v=zv;R;}
+ arr y;CVSWITCH(r.v,err(6),y=setUnique(v),err(16))
+ B rc=y.elements();
+ arr x;CVSWITCH(l.v,err(6),x=arr(v,lc,1),err(16))
+ y=arr(y,1,rc);
  z.v=arr(anyTrue(tile(x,1,(I)rc)==tile(y,(I)lc,1),1),lc);}
 
