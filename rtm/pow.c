cd8760b0f25e10cea33096e4a5d2910b60ec52d0
Aaron W. Hsu <arcfide@sacrideo.us>
2021-02-09 22:22:22 -0500

    Extra-safe

--- rtm/pow.c
+++ rtm/pow.c
@@ -6,7 +6,8 @@ MF(pow_o){if(fr){A t;A v=r;I flg;
   z=v;R;}
  if(rnk(ww))err(4);I c;CVSWITCH(ww.v,err(6),c=v.as(s32).scalar<I>(),err(11))
  z=r;DO(c,ll(z,z,e))}
-DF(pow_o){if(fr){A t;A v=r;I flg;
+DF(pow_o){if(!fl)err(2);
+ if(fr){A t;A v=r;I flg;
   do{A u;ll(u,l,v,e);rr(t,u,v,e);
    if(cnt(t)!=1)err(5);CVSWITCH(t.v,err(6),flg=v.as(s32).scalar<I>(),err(11))
    v=u;}while(!flg);
