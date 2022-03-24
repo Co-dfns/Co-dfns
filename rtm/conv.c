76d4076dfd348cf74785ab58fab7682a54a0818f
Aaron W. Hsu <arcfide@sacrideo.us>
2022-03-24 12:09:09 -0400

    Add convolve

---
+++ rtm/conv.c
@@ -0,0 +1,10 @@
+NM(conv,"conv",0,0,MT ,MT ,DFD,MT ,MT )
+DEFN(conv)
+DF(conv_f){
+ arr signal=unrav(std::get<arr>(r.v),r.s);arr filter=unrav(std::get<arr>(l.v),l.s);
+ dim4 stride(1,1,1,1);dim4 padding(0,0,0,0);dim4 dilation(1,1,1,1);
+ arr zv=af::convolve2NN(signal, filter, stride, padding, dilation);
+ dim4 s=zv.dims();I rk=zv.numdims();z.s=SHP(rk);DO(rk,z.s[i]=s[i]);
+ z.v=flat(zv);
+}
+ 
