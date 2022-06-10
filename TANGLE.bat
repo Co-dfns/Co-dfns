bdec0c6cae17337c8b41bf8756270ee2240be888
Aaron W. Hsu <arcfide@sacrideo.us>
2022-06-09 23:35:15 -0400

    Split out parsing tokenization and parsing; improve tangling

---
+++ TANGLE.bat
@@ -0,0 +1,2 @@
+set SH=C:\cygwin64\bin\bash.exe -l -c
+%SH% "cd $OLDPWD && ./TANGLE.sh"
--- TANGLE.sh
+++ TANGLE.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-echo "Tangling src/codfns.apln..."
-notangle codfns.nw > src/codfns.apln
 
+echo "Tangling codfns.apln..."
+notangle codfns.nw > src/codfns.apln
+echo "Tangling src/TEST.aplf..."
+notangle -R'[[TEST]]' codfns.nw > src/TEST.aplf
 echo "Tangling src/DISPLAY.aplf..."
 notangle -R'[[DISPLAY]] Utility' codfns.nw > src/DISPLAY.aplf
 
-echo "Tangling src/MK∆RTM.aplf..."
-notangle -R'[[MK∆RTM]] Command' codfns.nw > src/MK∆RTM.aplf
-
 echo "Tangling src/PP.aplf..."
 notangle -R'[[PP]] Utility' codfns.nw > src/PP.aplf
-
-echo "Tangling src/TANGLE.aplf..."
-notangle -R'[[TANGLE]] Command' codfns.nw > src/TANGLE.aplf
-
-echo "Tangling src/TEST.aplf..."
-notangle -R'[[TEST]] Function' codfns.nw > src/TEST.aplf
-
+echo "Tangling TANGLE.bat..."
+notangle -R'[[TANGLE.bat]]' codfns.nw > TANGLE.bat
+echo "Tangling TANGLE.aplf..."
+notangle -R'[[TANGLE]]' codfns.nw > src/TANGLE.aplf
+echo "Tangling WEAVE.sh..."
+notangle -R'[[WEAVE.sh]]' codfns.nw > WEAVE.sh
+echo "Tangling WEAVE.bat..."
+notangle -R'[[WEAVE.bat]]' codfns.nw > WEAVE.bat
 echo "Tangling src/WEAVE.aplf..."
-notangle -R'[[WEAVE]] Command' codfns.nw > src/WEAVE.aplf
+notangle -R'[[WEAVE]]' codfns.nw > src/WEAVE.aplf
+echo "Tangling src/MK∆RTM.aplf..."
+notangle -R'[[MK∆RTM]]' codfns.nw > src/MK∆RTM.aplf
 
 echo "Tangling TANGLE.sh..."
-notangle -R'Tangle Script' codfns.nw > TANGLE.sh
-
-echo "Tangling WEAVE.sh..."
-notangle -R'Weave Script' codfns.nw > WEAVE.sh
+notangle -R'[[TANGLE.sh]]' codfns.nw > TANGLE.sh
