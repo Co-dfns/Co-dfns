b53087bcb78d0d69f5023bffd95e5bd097b94464
Aaron W. Hsu <arcfide@sacrideo.us>
2022-09-08 10:36:32 -0400

    Drop LP

--- WEAVE.bat
+++
@@ -1,2 +0,0 @@
-set SH=C:\cygwin64\bin\bash.exe -l -c
-%SH% "cd $OLDPWD && ./WEAVE.sh"
--- WEAVE.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p woven
-noweave -delay -index codfns.nw > woven/codfns.tex
-cd woven
-xelatex --shell-escape codfns
-xelatex --shell-escape codfns
