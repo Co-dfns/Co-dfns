359c2c5be7a483a1b895fe8a09bca35cc4cf1b9e
Aaron W. Hsu <arcfide@sacrideo.us>
2022-10-24 22:47:11 -0400

    Fix issue with jot not accounting for functions that might overwrite their arguments.

--- rtm/jot.c
+++ rtm/jot.c
@@ -1,5 +1,11 @@
 ﻿OD(jot,"jot",(scm(l)&&scm(r)),(scd(l)&&scd(r)),MFD,DFD,MT ,MT )
 MF(jot_o){if(!fl){rr(z,aa,r,e);R;}if(!fr){ll(z,r,ww,e);R;}
- rr(z,r,e);ll(z,z,e);}
-DF(jot_o){if(!fl||!fr){err(2);}rr(z,r,e);ll(z,l,z,e);}
+ rr(z,r,e);
+ ll(z,z,e);
+ }
+DF(jot_o){if(!fl||!fr){err(2);}
+ A t;
+ rr(t,r,e);
+ ll(z,l,t,e);
+}
 
