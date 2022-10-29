e4e84973b7fda3f22c0e77b6e9c4352b6f936a13
Aaron W. Hsu <arcfide@sacrideo.us>
2022-10-29 04:50:52 -0400

    Add ⎕DR ibeam

--- rtm/ibeams.c
+++ rtm/ibeams.c
@@ -2,8 +2,7 @@
 
 int
 q_signal_func(struct cell_array **z, 
-    struct cell_array *l, struct cell_array *r,
-    struct cell_func *self)
+    struct cell_array *l, struct cell_array *r, struct cell_func *self)
 {
 	int err;
 	int32_t val;
@@ -38,3 +37,63 @@ q_signal_func(struct cell_array **z,
 
 struct cell_func q_signal_closure = {CELL_FUNC, 1, q_signal_func, 0};
 struct cell_func *q_signal_ibeam = &q_signal_closure;
+
+int
+q_dr_func(struct cell_array **z,
+    struct cell_array *l, struct cell_array *r, struct cell_func *self)
+{
+	struct cell_array *t;
+	int16_t *val;
+	int err;
+	
+	if (l)
+		return 16;
+	
+	if (err = mk_array(&t, ARR_SINT, STG_HOST, 0, NULL))
+		return err;
+	
+	val = t->values;
+	
+	switch (r->type) {
+	case ARR_SPAN:
+		*val = 323;
+		break;
+	case ARR_BOOL:
+		*val = 83;
+		break;
+	case ARR_SINT:
+		*val = 163;
+	case ARR_INT:
+		*val = 323;
+		break;
+	case ARR_DBL:
+		*val = 645;
+		break;
+	case ARR_CMPX:
+		*val = 1289;
+		break;
+	case ARR_CHAR8:
+		*val = 80;
+		break;
+	case ARR_CHAR16:
+		*val = 160;
+		break;
+	case ARR_CHAR32:
+		*val = 320;
+		break;
+	case ARR_MIXED:
+	case ARR_NESTED:
+		*val = 326;
+		break;
+	default:
+		release_cell(t);
+		return 99;
+	}
+	
+	*z = t;
+	
+	return 0;
+}
+
+struct cell_func q_dr_closure = {CELL_FUNC, 1, q_dr_func, 0};
+struct cell_func *q_dr_ibeam = &q_dr_closure;
