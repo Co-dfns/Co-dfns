d3e9cba0ae31ef35e528b8128403bf8e4cbb6e92
Aaron W. Hsu <arcfide@sacrideo.us>
2023-08-30 20:56:40 -0400

    Add max_vec fast path

--- rtm/ibeams.c
+++ rtm/ibeams.c
@@ -2483,3 +2483,71 @@ fail:
 }
 
 DECL_FUNC(min_vec, min_vec_func, error_dya_syntax)
+
+int
+max_vec_func(struct cell_array **z, struct cell_array *r, 
+    struct cell_func *self)
+{
+	struct cell_array *arr;
+	double real, imag;
+	int err;
+	
+	arr = NULL;
+	
+	switch (r->storage) {
+	case STG_DEVICE:{
+		CHKAF(af_max_all(&real, &imag, r->values), fail);
+	}break;
+	case STG_HOST:{
+		size_t count;
+		
+		real = imag = 0;
+		count = array_count(r);
+		
+		switch (r->type) {
+		ARR_SINT:{
+			int16_t *vals = r->values;
+			
+			for (size_t i = 0; i < count; i++)
+				real = max_real(real, vals[i]);
+		}break;
+		ARR_INT:{
+			int32_t *vals = r->values;
+			
+			for (size_t i = 0; i < count; i++)
+				real = max_real(real, vals[i]);
+		}break;
+		ARR_DBL:{
+			double *vals = r->values;
+			
+			for (size_t i = 0; i < count; i++)
+				real = max_real(real, vals[i]);
+		}break;
+		default:
+			CHK(99, fail, L"Unexpected array type");
+		}
+	}break;
+	default:
+		CHK(99, fail, L"Unknown storage device");
+	}
+	
+	if (real <= 1) {
+		CHKFN(mk_array_int8(&arr, (int8_t)real), fail);
+	} else if (real <= INT16_MAX) {
+		CHKFN(mk_array_int16(&arr, (int16_t)real), fail);
+	} else if (real <= INT32_MAX) {
+		CHKFN(mk_array_int32(&arr, (int32_t)real), fail);
+	} else {
+		CHKFN(mk_array_dbl(&arr, real), fail);
+	}
+	
+	*z = arr;
+	
+fail:
+	if (err)
+		release_array(arr);
+	
+	return err;
+}
+
+DECL_FUNC(max_vec, max_vec_func, error_dya_syntax)
