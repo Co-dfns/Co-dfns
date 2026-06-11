4b96bf7d3e35176c3cbb70755b6d2b810404d32d
Aaron W. Hsu <arcfide@sacrideo.us>
2026-06-11 01:30:43 -0400

    Update build.bat

--- rtm/build.bat
+++ rtm/build.bat
@@ -1,1 +1,3 @@
 cl /W4 /WX /LD /std:c17 /Zi /O2 /Fd"codfns.pdb" /I"%AF_PATH%\include" /D"NOMINMAX" codfns.c /link /LIBPATH:"%AF_PATH%\lib" /DYNAMICBASE "af.lib"
+for %%f in (codfns.dll codfns.lib codfns.pdb) do copy /Y %%f ..\tests\
+
